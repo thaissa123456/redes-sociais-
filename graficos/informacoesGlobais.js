5e26aabba848ace595cf9ad10a7343a135cbeedf
thaissa123456 <148073084+thaissa123456@users.noreply.github.com>
2024-10-16 10:13:29 -0300

    Update informacoesGlobais.js

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,1 +1,11 @@
+const url= ""
+
+async function visualizarinformacoesGlobais() {
+  const res = await fetch(url)
+  const dados = await res.json()
+  console.log(dados);
+}
+
+visualizarInformacoesGlabais()
+https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json
 
