13386c983f7fdff4824d50a8878ad84e0c204029
thaissa123456 <148073084+thaissa123456@users.noreply.github.com>
2024-10-23 10:23:00 -0300

    Update informacoesGlobais.js

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,11 +1,17 @@
-const url= ""
+const url= "https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json"
 
 async function visualizarinformacoesGlobais() {
   const res = await fetch(url)
   const dados = await res.json()
-  console.log(dados);
+  const pessoasConectadas =(dados.total_pessoas_conectadas/1e9)
+  const pessoasNoMundo = (dados.total_pessoas_mundo/1e9)
+  const horas = parseInt(dados.tempo_medio)
+  const minutos = math.round ((dados.tempo_medio - horas) * 100)
+  const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo) *100).toFixed(2)
+
+  const paragrafo = document.createElement('p')
 }
 
 visualizarInformacoesGlabais()
-https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json
+
 
